8179b3da2a18dff30451f62defe15321d4bea815
Yasser BERREHAIL <32455257+yasserberrehail@users.noreply.github.com>
2025-01-14 19:54:46 +0200

    Add files via upload

---
+++ companies.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Module Name: companies
+ * Description: A module to manage all companies for each country
+ * Author: hawala.pro
+ * Version: 1.0.0
+ */
+
+// Resto del código del módulo...
